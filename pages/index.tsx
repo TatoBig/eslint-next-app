2991b42021c071eab896c9370dd66ed74a102ee4
tatobig <tatobig@gmail.com>
2023-04-13 19:10:49 -0600

    Added husky configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,10 @@ import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const message = 'hello world';
+
 export default function Home() {
+  console.log(message);
   return (
     <>
       <Head>
